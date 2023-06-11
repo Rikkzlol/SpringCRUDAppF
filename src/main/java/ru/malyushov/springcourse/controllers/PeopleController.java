0a8cb4f2a71027b01aa7805e5900e5094b47f1f4
Rikkzlol <89975272+Rikkzlol@users.noreply.github.com>
2023-06-11 20:17:46 +0300

    -spring crud app(Add controller,DAO, model)

---
+++ src/main/java/ru/malyushov/springcourse/controllers/PeopleController.java
@@ -0,0 +1,35 @@
+package ru.malyushov.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.malyushov.springcourse.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index(Model model){
+        // получим всех людей из DAO и отобразим
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        // получичм одного человека по его id из DAO и пераддим на предтавлнеи
+        model.addAttribute("person",personDAO.show(id));
+        return "people/show";
+    }
+}
